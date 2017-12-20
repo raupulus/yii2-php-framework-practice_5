f2b36cf33aae00765e3f0a0db32b6f3deafd3f40
Raúl Caro Pastorino <tecnico@fryntiz.es>
2017-12-20 17:46:55 +0100

    Carga inicial

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'pgsql:host=localhost;dbname=fa',
+    'username' => 'fa',
+    'password' => 'fa',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
